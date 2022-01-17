9e3c84e9125f3a60cca5a580b0da636fb093aee8
Pablo Luiz <80119424+devpabloluiz@users.noreply.github.com>
2022-01-17 17:50:44 -0300

    Add files via upload

---
+++ Repeticao/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Boolean condicao = true;
+        int valor;
+        while(condicao == true)
+        {
+            Console.WriteLine("Digite um valor, 0 para sair");
+            valor = int.Parse(Console.ReadLine());            
+
+            if(valor == 0)
+            {   
+                Console.WriteLine("Você saiu da aplicação");
+                condicao = false;
+            }
+            else
+            {
+                Console.WriteLine("O valor informado é: " + valor);
+            }
+        }
+    }
+}
