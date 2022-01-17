a4c402b4c83a521ba137c003d89ea9ed73926089
Pablo Luiz <80119424+devpabloluiz@users.noreply.github.com>
2022-01-17 18:07:40 -0300

    Add files via upload

---
+++ Repeticao3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int[] lista = {1,2,3,4,5};
+
+        foreach(int numero in lista)
+        {
+            Console.WriteLine(numero);
+        }
+    }
+}
